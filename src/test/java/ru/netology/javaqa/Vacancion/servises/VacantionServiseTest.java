b2cce088480a0d82226703058ca2b8f903393b3f
Marya <masha.smelkowa@yandex.ru>
2024-05-22 12:22:33 +0300

    Initial commit

--- src/test/java/ru/netology/javaqa/Vacancion/servises/VacantionServiseTest.java
+++ src/test/java/ru/netology/javaqa/Vacancion/servises/VacantionServiseTest.java
@@ -1,10 +1,8 @@
 package ru.netology.javaqa.Vacancion.servises;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacantionServiseTest {
 @ParameterizedTest
